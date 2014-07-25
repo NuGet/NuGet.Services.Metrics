8ad020923815c7a694d33570c9657f47b98ac2a7
Deepak Aravindakshan <daravind@microsoft.com>
2014-07-25 16:08:46 -0700

    Added the website host to the metrics service

---
+++ src/NuGet.Services.Metrics.Website/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NuGet.Services.Metrics.Website")]
+[assembly: AssemblyDescription("Website that hosts the metrics service")]
+[assembly: AssemblyProduct("NuGet.Services.Metrics.Website")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97c42f9d-c8e1-4208-803c-dffa2b89b62a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/NuGet.Services.Metrics.Website/WebsiteStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using NuGet.Services.Metrics.Core;
+using System.Web.Configuration;
+using System.Diagnostics;
+
+[assembly: OwinStartup(typeof(NuGet.Services.Metrics.Website.WebsiteStartup))]
+
+namespace NuGet.Services.Metrics.Website
+{
+    public class WebsiteStartup
+    {
+        private PackageStatsHandler _packageStatsHandler;
+        private const string SqlConfigurationKey = "Metrics.SqlServer";
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var connectionStringSetting = WebConfigurationManager.ConnectionStrings[SqlConfigurationKey];
+            _packageStatsHandler = new PackageStatsHandler(connectionStringSetting.ConnectionString);
+            appBuilder.Run(Invoke);
+        }
+
+        private async Task Invoke(IOwinContext context)
+        {
+            var requestUri = context.Request.Uri;
+            Trace.TraceInformation("Request received : {0}", requestUri.AbsoluteUri);
+
+            await _packageStatsHandler.Invoke(context);
+            Trace.TraceInformation("Request accepted. Processing...");
+        }
+    }
+}
