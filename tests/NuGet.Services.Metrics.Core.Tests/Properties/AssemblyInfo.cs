6c99bc1caedd8c3e2ce9c6e8d5b91537627c93aa
deepakaravindr <daravind@microsoft.com>
2014-07-27 20:18:51 -0700

    Created a test project

---
+++ tests/NuGet.Services.Metrics.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NuGet.Services.Metrics.Core.Tests")]
+[assembly: AssemblyDescription("Tests for NuGet Metrics Service")]
+[assembly: AssemblyProduct("NuGet.Services.Metrics.Core.Tests")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cda4acc8-0e76-4f1c-93b9-987e7ccf3c4d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
