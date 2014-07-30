89ce64fd00c84d4fa010b29a6cdc22faef0ba5d9
deepakaravindr <daravind@microsoft.com>
2014-07-30 10:49:51 -0700

    Simplified Deploy.ps1

--- src/NuGet.Services.Metrics.Website/Deploy.ps1
+++ src/NuGet.Services.Metrics.Website/Deploy.ps1
@@ -13,34 +13,23 @@ Write-Host "Web Package Name: " $WebPackageName
 $WebPackagePath = $OctopusParameters['Octopus.Action.Package.CustomInstallationDirectory'] + '\' + $WebPackageName
 Write-Host "Web Package Path: " $WebPackagePath
 
-Write-Host "Getting azure subscriptions"
-$subscriptions = Get-AzureSubscription
-if(!$subscriptions)
+Write-Host "Looking for certificate in CurrentUser"
+$cert = dir cert:\CurrentUser  -rec | where { $_.Thumbprint -eq $AzureCertificateThumbPrint } | Select -First 1
+if(!$cert)
 {
-	Write-Host "Looking for certificate in CurrentUser"
-    $cert = dir cert:\CurrentUser  -rec | where { $_.Thumbprint -eq $AzureCertificateThumbPrint } | Select -First 1
-    if(!$cert)
-    {
-        Write-Host "Not Found in CurrentUser. Looking at LocalMachine"
-        $cert = dir cert:\LocalMachine  -rec | where { $_.Thumbprint -eq $AzureCertificateThumbPrint } | Select -First 1
-    }
-    
-    if(!$cert)
-    {
-        throw "Certificate is not found in CurrentUser or LocalMachine"
-    }
-    
-    Write-Host "Certificate was found. Setting azure subscription using the certificate..."
-    Set-AzureSubscription -SubscriptionName '$AzureSubscriptionName' -Certificate $cert -SubscriptionId $AzureSubscriptionId
-	Write-Host "Azure subscription was set successfully using the certificate obtained."
+    Write-Host "Not Found in CurrentUser. Looking at LocalMachine"
+    $cert = dir cert:\LocalMachine  -rec | where { $_.Thumbprint -eq $AzureCertificateThumbPrint } | Select -First 1
 }
-else
+
+if(!$cert)
 {
-    Write-Host "Azure Subscriptions already exist"
-    Write-Host $subscriptions
+    throw "Certificate is not found in CurrentUser or LocalMachine"
 }
-Write-Host "Selecting current azure subscription..."
+
+Write-Host "Certificate was found. Setting azure subscription using the certificate..."
+Set-AzureSubscription -SubscriptionName '$AzureSubscriptionName' -Certificate $cert -SubscriptionId $AzureSubscriptionId
+Write-Host "Azure subscription was set successfully using the certificate obtained. Selecting default azure subscription..."
 Select-AzureSubscription -SubscriptionName '$AzureSubscriptionName' -Default
-Write-Host "Selected current azure subscription. Publishing azure website..."
+Write-Host "Selected default azure subscription. Publishing azure website..."
 Publish-AzureWebsiteProject -Name $AzureWebsiteName -Package $WebPackagePath -Slot staging
 Write-Host "Published azure website successfully."
