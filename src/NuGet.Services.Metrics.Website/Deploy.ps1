8a770484e09a842add3750b26969cafe28db0e9c
deepakaravindr <daravind@microsoft.com>
2014-07-30 02:38:59 -0700

    Deploying Azure websites via octopus is painful

--- src/NuGet.Services.Metrics.Website/Deploy.ps1
+++ src/NuGet.Services.Metrics.Website/Deploy.ps1
@@ -1,3 +1,1 @@
-$AzureSubscriptionName = $OctopusParameters['Deployment.Azure.SubscriptionName']
-Select-AzureSubscription -SubscriptionName '$AzureSubscriptionName' -Current
 Publish-AzureWebsiteProject -Name $DeploymentAzureWebsiteName -Package $OctopusActionPackageCustomInstallationDirectory -Slot staging
