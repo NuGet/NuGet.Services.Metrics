fda8823485ba53660b283b7e42f96aaa389e11f4
deepakaravindr <daravind@microsoft.com>
2014-07-30 02:21:50 -0700

    Simplified Deploy.ps1

--- src/NuGet.Services.Metrics.Website/Deploy.ps1
+++ src/NuGet.Services.Metrics.Website/Deploy.ps1
@@ -1,17 +1,3 @@
-Write-Host "Trying to find Certificate with Thumbprint:"
-Write-Host $DeploymentAzureCertificateThumbprint
-$cert = dir cert:\LocalMachine  -rec | where { $_.Thumbprint -eq '$DeploymentAzureCertificateThumbprint' } | Select -First 1
-if(!$cert)
-{
-    Write-Host "Not Found in LocalMachine"
-    $cert = dir cert:\CurrentUser  -rec | where { $_.Thumbprint -eq '$DeploymentAzureCertificateThumbprint' } | Select -First 1
-}
-
-if(!$cert)
-{
-    throw "Certificate is not found"
-}
-
-Set-AzureSubscription -SubscriptionName '$DeploymentAzureSubscriptionName' -Certificate $cert -SubscriptionId $DeploymentAzureSubscriptionId
-Select-AzureSubscription -SubscriptionName '$DeploymentAzureSubscriptionName' -Current
+$AzureSubscriptionName = $OctopusParameters['Deployment.Azure.SubscriptionName']
+Select-AzureSubscription -SubscriptionName '$AzureSubscriptionName' -Current
 Publish-AzureWebsiteProject -Name $DeploymentAzureWebsiteName -Package $OctopusActionPackageCustomInstallationDirectory -Slot staging
