f4aae9cb5b9c1809ee71fd582f201b45eec98c09
Xavier Decoster <xavier.decoster@outlook.com>
2015-05-11 20:18:57 +0200

    Update license header on files.

--- src/NuGet.Services.Metrics.Website/WebsiteStartup.cs
+++ src/NuGet.Services.Metrics.Website/WebsiteStartup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using Microsoft.Owin;
 using NuGet.Services.Metrics.Core;
 using Owin;
 using System;
